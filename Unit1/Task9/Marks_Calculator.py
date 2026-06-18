1fd45bd9960dde6345a8c4f08c560b3ee806d091
Kusuma Vallabhaneni <kusumavallabhaneni64@gmail.com>
2026-06-18 14:59:24 +0530

    kusuma task 9

---
+++ Unit1/Task9/Marks_Calculator.py
@@ -0,0 +1,9 @@
+m1 = int(input("Enter Marks of Subject 1: "))
+m2 = int(input("Enter Marks of Subject 2: "))
+m3 = int(input("Enter Marks of Subject 3: "))
+m4 = int(input("Enter Marks of Subject 4: "))
+m5 = int(input("Enter Marks of Subject 5: "))
+
+total = m1 + m2 + m3 + m4 + m5
+
+print("Total Marks =", total)
