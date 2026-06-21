826e17916371694a1c5511789154bebcf251eeb7
Varsha-Kilaru <Varshakilaru16@gmail.com>
2026-06-21 17:39:21 +0530

    #Business Expense Calculator

---
+++ Unit1/task36/business_expense_calculator.py
@@ -0,0 +1,9 @@
+budget = float(input("Enter total budget: "))
+expenses = float(input("Enter total expenses: "))
+
+remaining_budget = budget - expenses
+
+print("\n----- Budget Summary -----")
+print("Total Budget:", budget)
+print("Total Expenses:", expenses)
+print("Remaining Budget:", remaining_budget)
