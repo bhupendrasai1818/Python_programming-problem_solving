c9d5178a34a0ea6d583ef6f1832cbecfa3c4d720
kusumavallabhaneni64 <kusumavallabhaneni64@gmail.com>
2026-06-21 18:41:27 +0530

    Utility Billing System.py

--- Unit1/Task45/Utility Billing System.py
+++ Unit1/Task45/Utility Billing System.py
@@ -4,5 +4,4 @@ gas_bill = float(input("Enter Gas Bill Amount: "))
 internet_bill = float(input("Enter Internet Bill Amount: "))
 
 total_bill = electricity_bill + water_bill + gas_bill + internet_bill
-
 print("Total Utility Bill =", total_bill)
