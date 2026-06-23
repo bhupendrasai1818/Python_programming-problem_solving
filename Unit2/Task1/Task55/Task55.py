a0a29908075a56bbec78a1d902fff72255acd18a
Kusuma Vallabhaneni <kusumavallabhaneni64@gmail.com>
2026-06-23 13:36:02 +0530

    deleted task55

--- Unit2/Task1/Task55/Task55.py
+++
@@ -1,8 +0,0 @@
-username = input("Enter Username: ")
-password = input("Enter Password: ")
-age = int(input("Enter Age: "))
-
-if len(username) >= 5 and len(password) >= 8 and age >= 18:
-    print("Registration Successful")
-else:
-    print("Registration Failed")
