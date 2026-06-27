6606b05d837c1608b1fc1c46def26f017eac39d4
Anusha <anushamallela23@gmail.com>
2026-06-27 12:59:36 +0530

    Login Authentication System

---
+++ Unit2/Task41/Login Authentication System.py
@@ -0,0 +1,12 @@
+username = "admin"
+password = "1234"
+
+user = input("Enter Username: ")
+pwd = input("Enter Password: ")
+
+if user == username and pwd == password:
+    print("Login Successful")
+    print("Access Granted")
+else:
+    print("Invalid Username or Password")
+    print("Access Denied")
